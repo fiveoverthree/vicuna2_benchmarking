6b3eb5a086e77d8fbc2599f70e715af854dc79c7
jjones <jefferson.jones@tuwien.ac.at>
2026-07-24 15:16:20 +0200

    add embench_iot for hybrid sim

--- benchmark_sources/embench_iot/CMake/add_test.cmake
+++ benchmark_sources/embench_iot/CMake/add_test.cmake
@@ -137,79 +137,71 @@ endmacro()
 #     message(STATUS "Successfully added ${TEST_NAME}")
 # endmacro()
 
-# macro(add_Benchmark_Hybrid TEST SOURCE_DIR TEST_BUILD_DIR CONFIG_SCRIPT)
-#     #Check if Gem5 simulator has been built.  If not build it. TODO: Currently, if changes are made to the rtl/verilator model, the gem5 build must be manually deleted to rebuild
-#     if(NOT EXISTS "${GEM5_MODEL_DIR}/gem5/build/ALL/gem5.opt"  OR (REBUILD_GEM5 AND NOT REBUILT))
-#         message("Gem5 executable not present!  Building it now.")
-#         set(REBUILT ON)
-#         # Make sure verilator C files have been built
-#         if(NOT EXISTS "${VERILATOR_MODEL_DIR}/build/CMakeFiles/verilated_model.dir/Vvproc_top.dir/")
-#             message(FATAL_ERROR "Verilator Model C Files not present!  Build the verilator model and try again.")
-#         endif()
-#         #${nproc} not parsing correctly?
-#         execute_process(COMMAND scons EXTRAS=${GEM5_MODEL_DIR}/vicuna2_simobject -j32 build/ALL/gem5.opt 
-#                             WORKING_DIRECTORY ${GEM5_MODEL_DIR}/gem5/)
-#     endif()
+macro(add_Benchmark_Hybrid TEST CONFIG_SCRIPT)
+    #Check if Gem5 simulator has been built.  If not build it.
+    build_gem5()
 
-#     set(TEST_NAME ${TEST}_Hybrid_${CONFIG_SCRIPT})
+    set(TEST_NAME ${TEST}_Hybrid_${CONFIG_SCRIPT})
 
-#     add_executable(${TEST_NAME})
+    add_executable(${TEST_NAME})
 
-#     target_include_directories(${TEST_NAME} PRIVATE
-#         ${SOURCE_DIR}
-#         ${SOURCE_DIR}/model_data
-#         ${FRAMEWORK_TOP}/
-#     )
+    file(GLOB EMBENCH_SRCS "${EMBENCH_IOT_TOP}/embench-iot/src/${TEST}/*.c")
+    set(EMBENCH_SRCS ${EMBENCH_SRCS} "${EMBENCH_IOT_TOP}/embench-iot/support/beebsc.c")
 
-#     target_sources(${TEST_NAME} PUBLIC
-#         ${FRAMEWORK_TOP}/main.cpp
-#         ${SOURCE_DIR}/${TEST}_data/${TEST}_input_data.cc
-#         ${SOURCE_DIR}/${TEST}_data/${TEST}_input_data.h
-#         ${SOURCE_DIR}/${TEST}_data/${TEST}_model_data.cc
-#         ${SOURCE_DIR}/${TEST}_data/${TEST}_model_data.h
-#         ${SOURCE_DIR}/${TEST}_data/${TEST}_model_settings.cc
-#         ${SOURCE_DIR}/${TEST}_data/${TEST}_model_settings.h
-#         ${SOURCE_DIR}/${TEST}_data/${TEST}_output_data_ref.cc
-#         ${SOURCE_DIR}/${TEST}_data/${TEST}_output_data_ref.h
-#     )
-#     #Set Linker
-#     target_link_options(${TEST_NAME} PRIVATE "-nostartfiles")
-#     #target_link_options(${TEST_NAME} PRIVATE "-nostdlib")
+    target_include_directories(${TEST_NAME} PUBLIC
+        ${EMBENCH_IOT_TOP}/embench-iot/support
+        ${EMBENCH_IOT_TOP}/embench-iot/src/${TEST}
+        ${EMBENCH_IOT_TOP}/benchmarks
+        ${FRAMEWORK_TOP}
+    )
+    
+    target_sources(${TEST_NAME} PUBLIC
+        ${FRAMEWORK_TOP}/main.cpp
+        ${FRAMEWORK_TOP}/vicuna2_bsp/crt0.S
+        ${EMBENCH_IOT_TOP}/benchmarks/benchmark.hpp   
+        ${EMBENCH_SRCS}
+    )
 
-#     target_link_options(${TEST_NAME} PRIVATE "-T${VICUNA_BSP_TOP}/lld_link.ld")
+    add_definitions(-DGLOBAL_SCALE_FACTOR=${GLOBAL_SCALE_FACTOR})
+
+    #Set Linker
+    target_link_options(${TEST_NAME} PRIVATE "-nostartfiles")
+    #target_link_options(${TEST_NAME} PRIVATE "-nostdlib")
+
+    target_link_options(${TEST_NAME} PRIVATE "-T${VICUNA_BSP_TOP}/lld_link.ld")
 
 
-#     #Link BSP
-#     target_link_libraries(${TEST_NAME} PRIVATE bsp_Vicuna UART_Vicuna tflm sim_hybrid)
+    #Link BSP
+    target_link_libraries(${TEST_NAME} PRIVATE UART_Vicuna sim_hybrid)
 
-#     add_custom_command(TARGET ${TEST_NAME}
-#                        POST_BUILD
-#                        COMMAND ${CMAKE_OBJDUMP} -D ${TEST_NAME}.elf > ${TEST_NAME}_dump.txt
-#                        )
+    add_custom_command(TARGET ${TEST_NAME}
+                       POST_BUILD
+                       COMMAND ${CMAKE_OBJDUMP} -D ${TEST_NAME}.elf > ${TEST_NAME}_dump.txt
+                       )
 
-#     #Hybrid Sim allows trace outputs, should be able to enable it here
-#     # set(INST_TRACE_ARGS "${BUILD_DIR}/Testing/inst_trace.txt")
+    #Hybrid Sim allows trace outputs, should be able to enable it here
+    # set(INST_TRACE_ARGS "${BUILD_DIR}/Testing/inst_trace.txt")
 
-#     # if(TRACE)
-#     #     set(MEM_TRACE_ARGS "${BUILD_DIR}/Testing/last_test_mem.csv")
-#     #     set(VCD_TRACE_ARGS "${BUILD_DIR}/Testing/last_test_sig.vcd")
+    # if(TRACE)
+    #     set(MEM_TRACE_ARGS "${BUILD_DIR}/Testing/last_test_mem.csv")
+    #     set(VCD_TRACE_ARGS "${BUILD_DIR}/Testing/last_test_sig.vcd")
 
-#     # else()
-#     #     set(MEM_TRACE_ARGS "")
-#     #     set(VCD_TRACE_ARGS "")
-#     # endif()
+    # else()
+    #     set(MEM_TRACE_ARGS "")
+    #     set(VCD_TRACE_ARGS "")
+    # endif()
                        
 	              
-#   #Add Test
-#         add_test(NAME ${TEST_NAME} 
-#              COMMAND ${GEM5_MODEL_DIR}/gem5/build/ALL/gem5.opt ${GEM5_MODEL_DIR}/configuration_scripts/${CONFIG_SCRIPT}.py ${BINARY_DIR}/${TEST_NAME}.elf
-#              WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+  #Add Test
+        add_test(NAME ${TEST_NAME} 
+             COMMAND ${GEM5_MODEL_DIR}/gem5/build/ALL/gem5.opt ${GEM5_MODEL_DIR}/configuration_scripts/${CONFIG_SCRIPT}.py ${BUILD_DIR}/benchmark_sources/embench_iot/${TEST_NAME}.elf ${MEM_W}
+             WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
 
-#     set_tests_properties(${TEST_NAME} PROPERTIES TIMEOUT 60) #TODO: Find a reasonable timeout for these tests
+    set_tests_properties(${TEST_NAME} PROPERTIES TIMEOUT 300) #TODO: Find a reasonable timeout for these tests
 
-#     message(STATUS "Successfully added ${TEST_NAME}")
+    message(STATUS "Successfully added ${TEST_NAME}")
 
-# endmacro()
+endmacro()
 
 macro(add_Benchmark_Spike TEST)
     #Build spike if it isnt present
@@ -242,7 +234,7 @@ macro(add_Benchmark_Spike TEST)
     #Set Linker
     target_link_options(${TEST_NAME} PRIVATE "-nostartfiles")
 
-    target_link_options(${TEST_NAME} PRIVATE "-T${VICUNA_BSP_TOP}/lld_link.ld") #Spike address space starts at 0x80000000, needs different linker script
+    target_link_options(${TEST_NAME} PRIVATE "-T${VICUNA_BSP_TOP}/lld_link.ld") 
     #Link tflm and spike sim libraries
     target_link_libraries(${TEST_NAME} PRIVATE sim_spike)   
 
