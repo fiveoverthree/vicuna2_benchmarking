191efcca984f947ef74230a5192e3b95671e53c0
Daniel Schloms <daniel.schloms@gmail.com>
2026-07-31 17:13:29 +0200

    Add address markers to ETISS and Verilator simulator framework

--- benchmark_sources/framework/etiss/simulator.hpp
+++ benchmark_sources/framework/etiss/simulator.hpp
@@ -4,53 +4,53 @@
 #include <cstdio>
 
 enum : uint32_t {
-    SH_SYS_EXIT = 0x20,                 // semihosting operation number
-    ADP_Stopped_ApplicationExit = 0x20026 // reason code in the arg block
+  SH_SYS_EXIT = 0x20,                   // semihosting operation number
+  ADP_Stopped_ApplicationExit = 0x20026 // reason code in the arg block
 };
 
 struct sh_exit_args {
-    uint32_t reason;     // field 1
-    uint32_t stopcode;   // field 2
+  uint32_t reason;   // field 1
+  uint32_t stopcode; // field 2
 };
 
-class Simulator
-{
+extern "C" {
+void __attribute__((noinline)) address_match_start() { volatile int a = 1; }
+void __attribute__((noinline)) address_match_end() { volatile int a = 1; }
+}
+
+class Simulator {
 public:
-    Simulator() = default;
+  Simulator() = default;
 
-    inline void begin_measurement() {}
-    inline void end_measurement() {}
+  inline void begin_measurement() { address_match_start(); }
+  inline void end_measurement() { address_match_end(); }
 
-    void terminate(int code)
-    {
-        sh_exit_args args = {
-            ADP_Stopped_ApplicationExit,
-            static_cast<uint32_t>(code)
-        };
-        riscv_semihosting_exit_extended(static_cast<int>(SH_SYS_EXIT), (void*)&args);
-    }
+  void terminate(int code) {
+    sh_exit_args args = {ADP_Stopped_ApplicationExit,
+                         static_cast<uint32_t>(code)};
+    riscv_semihosting_exit_extended(static_cast<int>(SH_SYS_EXIT),
+                                    (void *)&args);
+  }
 
-    static inline long riscv_semihosting_exit_extended(int reason, void* argpack)
-    {
-        register long a0 asm("a0") = reason;
-        register void* a1 asm("a1") = argpack;
-        register long ret asm("a0");
+  static inline long riscv_semihosting_exit_extended(int reason,
+                                                     void *argpack) {
+    register long a0 asm("a0") = reason;
+    register void *a1 asm("a1") = argpack;
+    register long ret asm("a0");
 
-        asm volatile(
-            ".option push            \n"
-            ".option norvc          \n"
-            ".balign 16             \n"
-            "slli x0, x0, 0x1f      \n"
-            "ebreak                 \n"
-            "srai x0, x0, 7         \n"
-            ".option pop            \n"
-            : "=r"(ret)             // output operand
-            : "r"(a0), "r"(a1)      // input operands
-            : "memory"
-        );
+    asm volatile(".option push            \n"
+                 ".option norvc          \n"
+                 ".balign 16             \n"
+                 "slli x0, x0, 0x1f      \n"
+                 "ebreak                 \n"
+                 "srai x0, x0, 7         \n"
+                 ".option pop            \n"
+                 : "=r"(ret)        // output operand
+                 : "r"(a0), "r"(a1) // input operands
+                 : "memory");
 
-        return ret; // not reached
-    }
+    return ret; // not reached
+  }
 
-    ~Simulator() = default;
+  ~Simulator() = default;
 };
--- benchmark_sources/framework/verilator/simulator.hpp
+++ benchmark_sources/framework/verilator/simulator.hpp
@@ -1,56 +1,55 @@
 #ifndef SIMULATOR_HPP
-#define SIMULATOR_HPP 
+#define SIMULATOR_HPP
 
-#include <cstdint>
 #include "uart.hpp"
-
-
 #include <cstdint>
-class Simulator
-{
-    private:
 
-    // Stats variables
-    uint32_t start_cycles, start_instr;
-    uint32_t end_cycles, end_instr;
+extern "C" {
+void __attribute__((noinline)) address_match_start() { volatile int a = 1; }
+void __attribute__((noinline)) address_match_end() { volatile int a = 1; }
+}
 
-    //Memory mapped address to terminate execution
-    volatile int* const terminate_addr = ( int*) 0x00000408;
+class Simulator {
+private:
+  // Stats variables
+  uint32_t start_cycles, start_instr;
+  uint32_t end_cycles, end_instr;
 
-    public:
+  // Memory mapped address to terminate execution
+  volatile int *const terminate_addr = (int *)0x00000408;
 
-    Simulator(){};
+public:
+  Simulator(){};
 
-    //Function to start measurement
-    //Verilator Simulation uses simulated CSRs
-    inline void begin_measurement()
-    {
-        asm volatile ("csrr %0,mcycle"   : "=r" (start_cycles)  );
-        asm volatile ("csrr %0,minstret"   : "=r" (start_instr)  );
-    };
+  // Function to start measurement
+  // Verilator Simulation uses simulated CSRs
+  inline void begin_measurement() {
+    asm volatile("csrr %0,mcycle" : "=r"(start_cycles));
+    asm volatile("csrr %0,minstret" : "=r"(start_instr));
+    address_match_start();
+  };
 
-    //Function to end measurement
-    //Verilator Simulation uses simulated CSRs
-    inline void end_measurement()
-    {
-        asm volatile ("csrr %0,mcycle"   : "=r" (end_cycles)  );
-        asm volatile ("csrr %0,minstret"   : "=r" (end_instr)  );
-        uart_printf("Total Cycles:       %d\n",end_cycles-start_cycles);
-        uart_printf("Total Instructions: %d\n\n",end_instr-start_instr);
-    };
+  // Function to end measurement
+  // Verilator Simulation uses simulated CSRs
+  inline void end_measurement() {
+    address_match_end();
+    asm volatile("csrr %0,mcycle" : "=r"(end_cycles));
+    asm volatile("csrr %0,minstret" : "=r"(end_instr));
+    uart_printf("Total Cycles:       %d\n", end_cycles - start_cycles);
+    uart_printf("Total Instructions: %d\n\n", end_instr - start_instr);
+  };
 
-    //Termination success or failure function for this simulator
-    void terminate(int code) // TODO: Int return codes
-    {
-        if (code == 0)
-        {
-            *terminate_addr = 0x0; //Write 0x0 to signal success
-        } else {
-            *terminate_addr = 0xf; //Write 0xF to signal success
-        }
-    };
+  // Termination success or failure function for this simulator
+  void terminate(int code) // TODO: Int return codes
+  {
+    if (code == 0) {
+      *terminate_addr = 0x0; // Write 0x0 to signal success
+    } else {
+      *terminate_addr = 0xf; // Write 0xF to signal success
+    }
+  };
 
-    //Cleanup any allocatations
-    ~Simulator(){};
+  // Cleanup any allocatations
+  ~Simulator(){};
 };
 #endif
